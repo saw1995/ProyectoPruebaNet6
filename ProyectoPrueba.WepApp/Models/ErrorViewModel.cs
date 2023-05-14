6933b19e332caf628b8963e8e2c631fa0f9aeac1
patrickdev <patrick2012_1@live.com.mx>
2023-05-14 16:20:25 -0400

    first commit

---
+++ ProyectoPrueba.WepApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoPrueba.WepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
